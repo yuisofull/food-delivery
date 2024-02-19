7300388bc4f5505ab418354825de7a3366cda992
yui <nhankiet0501@gmail.com>
2024-02-19 16:04:51 +0700

    remove waitgroup

--- component/asyncjob/job_manager.go
+++ component/asyncjob/job_manager.go
@@ -3,43 +3,34 @@ package asyncjob
 import (
 	"context"
 	"log"
-	"sync"
 )
 
 type group struct {
 	jobs         []Job
 	isConcurrent bool
-	wg           *sync.WaitGroup
 }
 
 func NewGroup(isConcurrent bool, jobs ...Job) *group {
 	return &group{
 		isConcurrent: isConcurrent,
 		jobs:         jobs,
-		wg:           new(sync.WaitGroup),
 	}
 }
 
 func (g *group) Run(ctx context.Context) error {
-	g.wg.Add(len(g.jobs))
-
 	errChan := make(chan error, len(g.jobs))
 
 	for i, _ := range g.jobs {
 		if g.isConcurrent {
 			go func(aj Job) {
 				errChan <- g.runJob(ctx, aj)
-				g.wg.Done()
 			}(g.jobs[i])
 			continue
 		}
 
 		job := g.jobs[i]
 		errChan <- g.runJob(ctx, job)
-		g.wg.Done()
 	}
-
-	g.wg.Wait()
 
 	for i := 1; i <= len(g.jobs); i++ {
 		if err := <-errChan; err != nil {
